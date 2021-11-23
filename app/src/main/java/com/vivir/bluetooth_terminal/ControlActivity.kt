51a05fdd376617f539e5f3baf182f68cd3fec117
Vivirinter <vivirequesty@gmail.com>
2021-11-23 11:28:09 +0300

    update

--- app/src/main/java/com/vivir/bluetooth_terminal/ControlActivity.kt
+++ app/src/main/java/com/vivir/bluetooth_terminal/ControlActivity.kt
@@ -1,5 +1,8 @@
 package com.vivir.bluetooth_terminal
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,11 +16,28 @@ import com.vivir.bluetooth_terminal.databinding.ActivityControlBinding
 class ControlActivity : AppCompatActivity() {
     private lateinit var binding: ActivityControlBinding
     private lateinit var actListLauncher: ActivityResultLauncher<Intent>
+    lateinit var bluetoothConnect: BluetoothConnect
+    private var listItem: ListItem? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_control)
         setContentView(binding.root)
         onBluetoothListResult()
+        init()
+        binding.apply {
+            bA.setOnClickListener {
+                bluetoothConnect.sendMessage("A")
+            }
+            bB.setOnClickListener {
+                bluetoothConnect.sendMessage("B")
+            }
+        }
+    }
+
+    private fun init() {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = bluetoothManager.adapter
+        bluetoothConnect = BluetoothConnect(bluetoothAdapter)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -31,7 +51,9 @@ class ControlActivity : AppCompatActivity() {
             actListLauncher.launch(Intent(this, BluetoothListActivity::class.java))
 
         } else if (item.itemId == R.id.id_connect) {
-
+            listItem.let {
+                bluetoothConnect.connect(it?.mac!!)
+            }
         }
         return super.onOptionsItemSelected(item)
     }
@@ -40,10 +62,9 @@ class ControlActivity : AppCompatActivity() {
         actListLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 if (it.resultCode == RESULT_OK) {
-                    Log.d(
-                        "Log: ",
-                        "Name: ${(it.data?.getSerializableExtra(BluetoothListActivity.DEVICE_KEY) as ListItem).name}"
-                    )
+                    listItem =
+                        it.data?.getSerializableExtra(BluetoothListActivity.DEVICE_KEY) as ListItem
+
                 }
             }
     }
